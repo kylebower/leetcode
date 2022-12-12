ac89aab5ff50ad6cdb39f5e410848110849d0e67
Kyle Bower <kylewbower@gmail.com>
2022-12-12 00:55:28 -0500

    Time: 68 ms (10.27%), Space: 13.8 MB (56.17%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -8,14 +8,13 @@ class Solution:
         n = 0
         reached_end_of_list = 0
         head_curr = head
+        array = []
         while reached_end_of_list == 0:
+            array.append(head_curr)
             n +=1
             if head_curr.next == None:
                 reached_end_of_list = 1
             else:
-                head_curr = head_curr.next
-        middle_node = head
-        for i in range(floor(n/2)):
-            middle_node = middle_node.next
-        return middle_node
+                head_curr = head_curr.next        
+        return array[floor(n/2)]
         
