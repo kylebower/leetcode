8cb939cd26846166a46727ed410e2ede89b6f33b
Kyle Bower <kylewbower@gmail.com>
2022-12-12 18:59:25 -0500

    Time: 98 ms (61.10%), Space: 14.2 MB (55.05%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,11 +1,9 @@
 class Solution:
-    def canConstruct(self, ransomNote: str, magazine: str) -> bool:        
-        result = True
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:       
         for x in ransomNote:
             location_x = magazine.find(x)
             if location_x == -1:
-                result = False
-                break
+                return False
             else:
                 magazine = magazine[:location_x] + magazine[location_x+1:]
-        return result
+        return True
