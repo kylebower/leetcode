f8a447ea501e312e80ca22da16741d69cce14b78
Kyle Bower <kylewbower@gmail.com>
2022-12-21 02:30:02 -0500

    Time: 151 ms (13.80%), Space: 14.2 MB (41.39%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        n = len(nums)
+        result = [0]*n
+        for i in range(n):
+            if i % 2 == 0:
+                result[i] = nums[int(i/2)]
+            else:
+                result[i] = nums[int((i-1)/2+n/2)]
+        return result
+    
+    # time complexity: O(n)
+    # space complexity: O(1)
