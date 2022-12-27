aeb6f11c0dd82859b16302b4e1526a32aa1ec3a9
Kyle Bower <kylewbower@gmail.com>
2022-12-27 01:24:38 -0500

    Time: 47 ms (80.77%), Space: 14.3 MB (55.57%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -3,38 +3,35 @@ class Solution:
         
         # base case
         n = len(nums)
-        if n == 1:
-            if nums[0] == target:
-                result = 0
-            else:
-                result = -1
-            return result
+        # if n == 1:
+        #     if nums[0] == target:
+        #         result = 0
+        #     else:
+        #         result = -1
+        #     return result
         
         # define pointers
         L, R = 0, n-1
         
         # perform binary search
-        if nums[L] > target and nums[R] < target:
-            return -1
+#         if nums[L] > target and nums[R] < target:
+#             return -1
         
-        if nums[L] == target:
-            return L
-        elif nums[R] == target:
-            return R
+#         if nums[L] == target:
+#             return L
+#         elif nums[R] == target:
+#             return R
             
-        while L < R:
+        while L <= R:
             mid = int(L + (R-L)/2)
             if nums[mid] == target:
                 return mid
-            elif nums[L] == target:
-                return L
-            elif nums[R] == target:
-                return R
-            # print("L: " + str(L))
-            # print("R: " + str(R))
-            # print("mid: " + str(mid))
+            # elif nums[L] == target:
+            #     return L
+            # elif nums[R] == target:
+            #     return R
             
-            if nums[L] < nums[mid]:
+            if nums[L] <= nums[mid]:
                 if nums[L] <= target and target < nums[mid]:
                     R = mid - 1
                 else:
@@ -44,31 +41,6 @@ class Solution:
                     L = mid + 1
                 else:
                     R = mid - 1
-            
-            
-            # if nums[L] < target and target < nums[mid]:
-            #     R = mid
-            # elif nums[mid] < target and target < nums[R]:
-            #     L = mid
-            # elif nums[mid] > target and target < nums[R]:
-            #     L = mid
-            # elif nums[L] < target and target > nums[mid]:
-            #     R = mid
-            # else:
-            #     L += 1
-            # if (nums[R] > target and nums[mid] < target) or (nums[R] > target and nums[mid] > nums[R]):
-            #     L = mid
-            # elif (nums[mid] > target and nums[L] < target) or (nums[L] < target and nums[mid] < nums[L]):
-            #     R = mid
-            # else:
-            #     return mid
         
         # return index of target if it is in nums, or -1 if it is not in nums
-        # if nums[L] == target:
-        #     result = L
-        # elif nums[R] == target:
-        #     result = R
-        # else:
-        #     result = -1
-        # return result
         return -1
