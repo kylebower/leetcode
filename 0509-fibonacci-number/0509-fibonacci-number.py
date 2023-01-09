5b861c13aa6121198823c0b175989da4f40a6344
Kyle Bower <kylewbower@gmail.com>
2023-01-08 21:19:14 -0500

    Time: 24 ms (98.41%), Space: 13.9 MB (52.57%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,9 +1,14 @@
 class Solution:
+    def __init__(self):
+        self.memo = {}
     def fib(self, n: int) -> int:
         
         # base cases
         if n == 0 or n == 1:
             return n
         
+        if n-1 not in self.memo: self.memo[n-1] = self.fib(n-1)
+        if n-2 not in self.memo: self.memo[n-2] = self.fib(n-2)
+        
         # return sum of the two preceding Fibonacci numbers
-        return self.fib(n-1) + self.fib(n-2)
+        return self.memo[n-1] + self.memo[n-2]
