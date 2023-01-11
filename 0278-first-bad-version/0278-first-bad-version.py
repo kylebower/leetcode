17456b1205a4c897bce557a3db0675f6a45a7346
Kyle Bower <kylewbower@gmail.com>
2023-01-11 12:15:55 -0500

    Time: 31 ms (83.94%), Space: 13.9 MB (11.93%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -0,0 +1,26 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        
+        # define pointers
+        L = 1
+        R = n+1
+        
+        # binary search to find first bad version
+        while L + 1 < R:
+            mid = int((L+R)/2)
+            if isBadVersion(mid):
+                R = mid
+            else:
+                L = mid
+        
+        # return first bad version
+        if isBadVersion(L):
+            return L
+        else:
+            return R
+
+        # time complexity: O(logn)
+        # space complexity: O(1)
