fef4d7098b5bc5a52a57b332d166de079345bd4b
Kyle Bower <kylewbower@gmail.com>
2023-01-11 11:41:07 -0500

    Time: 642 ms (79.11%), Space: 27.2 MB (74.68%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,21 +1,16 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         # define dictionary of numbers and latest occurance
-        new_dict = {}
+        dic = {}
         
-        n = len(nums)
         # iterate through nums
-        for i in range(n):
-            if new_dict.get(nums[i],-1) != -1:
-                if i - new_dict.get(nums[i]) <= k:
-                    # return True if condition satisfied
-                    return True
-                else:
-                    # update latest occurance of nums[i] in dict
-                    new_dict[nums[i]] = i
+        for i, v in enumerate(nums):
+            if v in dic and i - dic[v] <= k:
+                # return True if condition satisfied
+                return True
             else:
                 # add latest occurance of nums[i] to dict
-                new_dict[nums[i]] = i
+                dic[v] = i
         
         # return False if no such numbers exist in nums
         return False
