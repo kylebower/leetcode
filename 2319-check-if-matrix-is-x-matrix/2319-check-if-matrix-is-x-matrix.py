d17f88a802735597c3c5fb95f17dbe5f7f2765c4
Kyle Bower <kylewbower@gmail.com>
2023-01-12 17:50:57 -0500

    Time: 273 ms (91.95%), Space: 15.1 MB (20.12%) - LeetHub

--- 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.py
+++ 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.py
@@ -5,7 +5,7 @@ class Solution:
             if grid[i][i] == 0 or grid[i][n-i-1] == 0:
                 return False
             j = min(i, n-i-1)
-            if sum(grid[i][:j]+grid[i][j+1:n-j-1]+grid[i][n-j:]) != 0:
+            if max(grid[i][:j]+grid[i][j+1:n-j-1]+grid[i][n-j:]) > 0:
                 return False
             
         return True
