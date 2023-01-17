86fc2f015bd87e4696b52ebbf081e779fbcf1a0f
Kyle Bower <kylewbower@gmail.com>
2023-01-16 22:17:04 -0500

    Time: 44 ms (53.75%), Space: 13.8 MB (69.75%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,20 +1,14 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        # define dictionaries of characters in strings s and t
-        ds = {}
-        dt = {}
         
-        # populate dictionaries
-        for c in s:
-            ds[c] = ds.get(c,0)+1
-        for c in t:
-            dt[c] = dt.get(c,0)+1
+        c = 0
+        for cs in s:
+            c ^= ord(cs)  #ord is ASCII value
         
-        # return letter added
-        for c in dt:
-            if not c in ds:
-                return c
+        for ct in t:
+            c ^= ord(ct)
         
-        for c in ds:
-            if ds[c] != dt[c]:
-                return c
+        return chr(c) # convert from ASCII to string
+
+    # time complexity: O(n)
+    # space complexity: O(1)
