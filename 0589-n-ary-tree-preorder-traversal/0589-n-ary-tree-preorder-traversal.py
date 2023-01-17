29f09033c54c38e13950efd2c3b5ee64aa100214
Kyle Bower <kylewbower@gmail.com>
2023-01-16 22:53:58 -0500

    Time: 55 ms (71.22%), Space: 16.4 MB (12.84%) - LeetHub

---
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.py
@@ -0,0 +1,29 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        
+        # define output list
+        output = []
+        
+        # recursively perform depth first search to update output
+        self.dfs(root, output)
+        
+        # return preorder traversal of its nodes' values
+        return output
+        
+    def dfs(self, root, output):
+        if root is None:
+            return
+        
+        output.append(root.val)
+        
+        for child in root.children:
+            self.dfs(child, output)
+            
