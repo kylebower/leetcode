6990e1e5b49506357ed3f1e5ce75251df391733a
Kyle Bower <kylewbower@gmail.com>
2023-01-19 14:38:33 -0500

    Time: 34 ms (74.18%), Space: 13.9 MB (62.55%) - LeetHub

--- 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.py
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.py
@@ -1,11 +1,14 @@
 class Solution:
     def freqAlphabets(self, s: str) -> str:
-        dic1 = {'1': 'a', '2': 'b', '3': 'c', '4': 'd', '5': 'e', '6': 'f', '7': 'g', '8': 'h', '9': 'i'}
-        dic2 = {'10#': 'j', '11#': 'k', '12#': 'l', '13#': 'm', '14#': 'n', '15#': 'o', '16#': 'p', '17#': 'q', '18#': 'r', '19#': 's', '20#': 't', '21#': 'u', '22#': 'v', '23#': 'w', '24#': 'x', '25#': 'y', '26#': 'z'}
+        res = []
+        i = 0
+        while i < len(s):
+            if i+2 < len(s) and s[i+2]=='#':
+                c = chr(int(s[i:i+2])+96) # chr(97-122) is 'a' to 'z'
+                i += 3
+            else:
+                c = chr(int(s[i])+96)
+                i += 1
+            res.append(c)
         
-        for c in list(dic2.keys()):
-            s = s.replace(c,dic2[c])
-        for c in list(dic1.keys()):
-            s = s.replace(c,dic1[c])
-        
-        return s
+        return "".join(res)
