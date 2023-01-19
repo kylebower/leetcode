82f6cf8dc453f2ebaad197cb1082a300d00b797e
Kyle Bower <kylewbower@gmail.com>
2023-01-19 14:09:22 -0500

    Time: 355 ms (5.01%), Space: 48.2 MB (5.07%) - LeetHub

---
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.py
@@ -0,0 +1,22 @@
+import numpy as np
+class Solution:
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        # mat is an mxn matrix
+        m = len(mat)
+        n = len(mat[0])
+        
+        # base case: impossible params
+        if r*c != m*n:
+            return mat
+        
+        # define reshaped matrix
+        reshaped_mat = [[0]*c]*r
+        
+        print(mat[:])
+        # populate reshaped matrix
+        for i in range(r):
+            reshaped_mat[i] = np.array(mat).flatten()[0+i*c:c+i*c]
+        
+        # return new reshaped matrix
+        return reshaped_mat
+        
