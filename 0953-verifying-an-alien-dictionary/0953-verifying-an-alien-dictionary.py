96937016e33fd7eeca8149e3adae4b41420f9ffc
Kyle Bower <kylewbower@gmail.com>
2023-01-20 17:24:59 -0500

    Time: 37 ms (79.97%), Space: 13.9 MB (32.75%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -1,23 +1,29 @@
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
+        # base case
+        if len(words) < 2:
+            return True
+        
+        # define alien dictionary
         alien_dict = {}
         i = 0
         for c in order:
             alien_dict[c] = i
             i += 1
         
-        for j in range(len(words)-1):
-            if not self.inOrder(words[j], words[j+1], alien_dict):
+        # translate alien words into number
+        new_words = []
+        for w in words:
+            new_word = []
+            for ch in w:
+                new_word.append(alien_dict[ch])
+            new_words.append(new_word)
+        
+        # return false if any two words are out of order
+        for w1, w2 in zip(new_words, new_words[1:]):
+            if w2 < w1:
                 return False
         
+        # return True if words are sorted lexicographically in this alien language
         return True
-    
-    def inOrder(self, word1, word2, alien_dict):
-        min_len = min(len(word1), len(word2))
-        for i in range(min_len):
-            if alien_dict[word1[i]] > alien_dict[word2[i]]:
-                return False
-            elif alien_dict[word1[i]] < alien_dict[word2[i]]:
-                return True        
-        return len(word1) <= len(word2)
         
