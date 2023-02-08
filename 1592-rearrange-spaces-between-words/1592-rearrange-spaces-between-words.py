98b4611e558fc9d94aca9260b6ab88207502ca48
Kyle Bower <kylewbower@gmail.com>
2023-02-08 16:28:52 -0500

    Time: 29 ms (82.14%), Space: 13.8 MB (97.72%) - LeetHub

--- 1592-rearrange-spaces-between-words/1592-rearrange-spaces-between-words.py
+++ 1592-rearrange-spaces-between-words/1592-rearrange-spaces-between-words.py
@@ -1,34 +1,11 @@
 class Solution:
     def reorderSpaces(self, text: str) -> str:
-        num_spaces = 0
-        for c in text:
-            if c == " ":
-                num_spaces += 1
+        words = text.split()
+        nWords = len(words)
+        nSpaces = text.count(' ')
         
-        num_words = 0
-        result = []
-        split_text = text.split(' ')
-        for w in split_text:
-            if w != '':
-                result.append(w)
-                num_words += 1
+        gap = 0 if nWords < 2 else nSpaces // (nWords - 1)
+        end_space = nSpaces - gap * (nWords - 1)
         
-        if num_words < 2:
-            num_spaces_between_words = 0
-        else:
-            num_spaces_between_words = int(num_spaces // (num_words-1))
-        num_spaces_at_end = num_spaces - num_spaces_between_words * (num_words - 1)
+        return (" " * gap).join(words) + (" " * end_space)
         
-        seperator = ""
-        print(num_spaces_between_words)
-        for _ in range(num_spaces_between_words):
-            seperator += " "
-        
-        
-        end_space = ""
-        print(num_spaces_at_end)
-        for _ in range(num_spaces_at_end):
-            end_space += " "
-        
-        
-        return seperator.join(result) + end_space
