f1f9f3d163fe3b62acd34d1899ab0c58ae36e507
Kyle Bower <kylewbower@gmail.com>
2023-02-16 12:35:21 -0500

    Time: 52 ms (83.52%), Space: 13.9 MB (35.02%) - LeetHub

---
+++ 1436-destination-city/1436-destination-city.py
@@ -0,0 +1,17 @@
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        # make dict where key-value pair represents direct path from key to value city
+        d = {}
+        for p in paths:
+            d[p[0]] = p[1]
+            
+        # iterate until we arrive at destination
+        current_location = paths[0][0]
+        while current_location in d:
+            current_location = d[current_location]
+        
+        # return destination city
+        return current_location
+    
+    # time complexity: O(n)
+    # space complexity: O(n)
