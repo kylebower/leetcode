d81587b78f756f31160624364c780182044c182f
Kyle Bower <kylewbower@gmail.com>
2023-03-09 14:21:12 -0500

    Time: 43 ms (37.56%), Space: 14 MB (63.50%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -6,5 +6,13 @@ class Solution:
             arr_dict[n] = arr_dict.get(n,0) + 1
         
         # return true if the number of occurrences of each value in the array is unique or false otherwise
-        return len(list(arr_dict.values())) == len(set(arr_dict.values()))
+        set_num_occ = set()
+        for value in list(arr_dict.values()):
+            if value in set_num_occ:
+                return False
+            else:
+                set_num_occ.add(value)
+        
+        return True
+        # return len(list(arr_dict.values())) == len(set(arr_dict.values()))
     
