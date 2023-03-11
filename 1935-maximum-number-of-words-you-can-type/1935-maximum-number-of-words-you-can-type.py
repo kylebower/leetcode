a686dcd23cbdab41c00edd652503de8d7d2cef66
Kyle Bower <kylewbower@gmail.com>
2023-03-10 19:15:08 -0500

    Time: 46 ms (10.38%), Space: 14 MB (17.67%) - LeetHub

---
+++ 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.py
@@ -0,0 +1,19 @@
+class Solution:
+    def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
+        
+        res = 0
+        
+        for w in text.split(' '):
+            new_word = True
+            d = {}
+            for c in w:
+                d[c] = d.get(c,0) + 1
+            for c in brokenLetters:
+                if c in d.keys():
+                    new_word = False
+                    # continue
+            if new_word:
+                res += 1
+        
+        return res
+            
