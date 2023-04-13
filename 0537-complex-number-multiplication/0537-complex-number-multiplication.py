275d66684b6b18703d45578877e6bc7838082519
Kyle Bower <kylewbower@gmail.com>
2023-04-13 19:34:23 -0400

    Time: 27 ms (88.69%), Space: 13.8 MB (55.48%) - LeetHub

--- 0537-complex-number-multiplication/0537-complex-number-multiplication.py
+++ 0537-complex-number-multiplication/0537-complex-number-multiplication.py
@@ -1,11 +1,10 @@
 class Solution:
     def complexNumberMultiply(self, num1: str, num2: str) -> str:
-        n1_str = num1.split('+')
-        n2_str = num2.split('+')
+        n1_str = num1[:-1].split('+')
+        n2_str = num2[:-1].split('+')
         a = int(n1_str[0])
-        b = int(n1_str[1][:len(n1_str[1])-1])
+        b = int(n1_str[1])
         c = int(n2_str[0])
-        d = int(n2_str[1][:len(n2_str[1])-1])
-        result = str(int(a*c-b*d)) + "+" + str(int(b*c+a*d)) + "i"
-        return result
+        d = int(n2_str[1])
+        return "%d+%di" % (a*c-b*d, b*c+a*d)
         
