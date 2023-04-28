e31a9ecb4b97b801ba4da303a30ec9136fca8dce
Kyle Bower <kylewbower@gmail.com>
2023-04-28 01:48:05 -0400

    Time: 49 ms (5.17%), Space: 16.3 MB (20.97%) - LeetHub

--- 1023-camelcase-matching/1023-camelcase-matching.py
+++ 1023-camelcase-matching/1023-camelcase-matching.py
@@ -3,25 +3,30 @@ class Solution:
         
         res = []
         for q in queries:
+            print(q)
             res.append(self.match(q, pattern))
-        
         return res
     
-    def match(self, s: str, pattern: str):
-        n = len(s)
-        cur = 0
-        for c in pattern:
-            while cur < n and s[cur] != c:
-                if ord(s[cur]) >= ord('A') and ord(s[cur]) <= ord('Z'):
+    def isUpper(self, c):
+        return ord(c) >= ord('A') and ord(c) <= ord('Z')
+    
+    def match(self, q: str, pattern: str):        
+        # define two pointers
+        p1, p2 = 0, 0
+        while p1 < len(q) and p2 < len(pattern):
+            while p1 < len(q) and q[p1] != pattern[p2]:
+                if self.isUpper(q[p1]):
                     return False
-                cur += 1
-            if cur == n:
-                return False
-            cur += 1
-        
-        for k in range(cur,n):
-            c = s[k]
-            if ord(c) >= ord('A') and ord(c) <= ord('Z'):
-                return False
+                else:
+                    p1 += 1
+            if p1 < len(q) and p2 < len(pattern):
+                p2 += 1
+                p1 += 1
+        if p2 < len(pattern):
+            return False
+        while p1 < len(q):
+            if self.isUpper(q[p1]):
+                    return False
+            p1 += 1
         return True
                 
