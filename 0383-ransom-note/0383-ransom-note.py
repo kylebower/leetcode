99d1dc95f05832b1db6c4993733a17bab491d719
Kyle Bower <kylewbower@gmail.com>
2023-05-07 13:44:39 -0400

    Time: 80 ms (23.32%), Space: 16.6 MB (8.94%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,16 +1,12 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         magazineDictionary = {}
-        for i in range(len(magazine)):
-            c = magazine[i]
-            c_count = magazineDictionary.get(c, 0)
-            magazineDictionary[c] = c_count + 1
+        for c in magazine:
+            magazineDictionary[c] = magazineDictionary.get(c,0)+1
         
-        for i in range(len(ransomNote)):
-            c = ransomNote[i]
-            c_count = magazineDictionary.get(c, 0)
-            if c_count <= 0:
+        for c in ransomNote:
+            magazineDictionary[c] = magazineDictionary.get(c,0)-1
+            if magazineDictionary[c] < 0:
                 return False
-            magazineDictionary[c] = c_count - 1
             
         return True
