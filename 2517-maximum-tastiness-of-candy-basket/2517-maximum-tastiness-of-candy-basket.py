35fcc26322abfaafc0a74bb64675766d7dc6c960
Kyle Bower <kylewbower@gmail.com>
2023-05-19 14:04:31 -0400

    Time: 1174 ms (96.80%), Space: 29.3 MB (7.60%) - LeetHub

---
+++ 2517-maximum-tastiness-of-candy-basket/2517-maximum-tastiness-of-candy-basket.py
@@ -0,0 +1,32 @@
+class Solution:
+    def maximumTastiness(self, price: List[int], k: int) -> int:
+        # use binary search to find max tastiness of a candy basket
+        
+        # define two pointers for bounds on step size
+        L = 0
+        R = max(price)
+        
+        # sort prices
+        price.sort()
+        
+        # perform binary search
+        while L+1 < R:
+            mid = int(L+(R-L)/2)
+            if self.possible(price, mid, k):
+                L = mid
+            else:
+                R = mid
+        if self.possible(price, R, k):
+            return R
+        else:
+            return L
+        
+    def possible(self, price: List[int], stepSize: int, k: int) -> bool:
+        count = 0
+        cur = 0
+        for p in price:
+            if p >= cur:
+                count += 1
+                cur = p + stepSize
+        return count >= k 
+        
