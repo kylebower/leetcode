c33d2f4dd8b62e7a9253bc8af7c51bb0308f057d
Kyle Bower <kylewbower@gmail.com>
2023-06-03 13:56:12 -0400

    Time: 66 ms (39.30%), Space: 16.4 MB (50.68%) - LeetHub

---
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxSatisfaction(self, satisfaction: List[int]) -> int:
+        
+        # sort dishes by satisfaction
+        satisfaction.sort(reverse=1)
+        
+        # sum of satisfaction so far
+        sumSoFar = 0
+        
+        # current satisfaction
+        cur = 0
+        
+        # maximum sum of like-time coefficient
+        best = 0
+        
+        # iterate to find maximum sum of like-time coefficient
+        for i, dish in enumerate(satisfaction):
+            cur += sumSoFar + dish
+            sumSoFar += dish
+            best = max(cur,best)
+        
+        # return maximum sum of like-time coefficient
+        return best
+        
