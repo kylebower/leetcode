47c40aa415f7b75c7982240dd42ef07d730d476f
Kyle Bower <kylewbower@gmail.com>
2023-07-01 13:59:13 -0400

    Time: 202 ms (92.01%), Space: 18 MB (89.48%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.py
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -1,30 +1,12 @@
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
-        # base case:
-        # return false if hand can not be rearranged into groups of size groupSize
-        n = len(hand)
-        if n%groupSize != 0:
-            return False
-        
-        # sort hand
-        hand.sort()
-        
-        # make dict of occurences of each card in hand
-        d = {}
-        for card in hand:
-            d[card] = d.get(card,0) + 1
-            
-        # rearrange the cards into groups of straights
-        while len(d) > 0:
-            start = min(list(d.keys()))
-            for k in range(groupSize):
-                if start+k not in d:
-                    return False
-                else:
-                    d[start+k] = d.get(start+k)-1
-                    if d[start+k] == 0:
-                        del d[start+k]
-        
-        # return True if she can rearrange the cards
+        c = collections.Counter(hand)
+        for card in sorted(c):
+            cur = c[card]
+            if cur > 0:
+                for k in range(groupSize):
+                    c[card+k] -= cur
+                    if c[card+k] < 0:
+                        return False
         return True
     
