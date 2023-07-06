e73e45d664041a7c60c090691e096c6efeb66dad
Kyle Bower <kylewbower@gmail.com>
2023-07-06 13:35:33 -0400

    Time: 63 ms (83.99%), Space: 17.4 MB (66.23%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.py
+++ 0414-third-maximum-number/0414-third-maximum-number.py
@@ -1,8 +1,24 @@
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
-        distinct = sorted(set(nums), reverse = True)
-        if len(distinct) >= 3:
-            return distinct[2]
-        else:
-            return distinct[0]
+        max1 = None
+        max2 = None
+        max3 = None
         
+        for n in nums:
+            if n == max1 or n == max2 or n == max3:
+                continue
+            elif max1 == None or n > max1:
+                max3 = max2
+                max2 = max1
+                max1 = n
+            elif max2 == None or n > max2:
+                max3 = max2
+                max2 = n
+            elif max3 == None or n > max3:
+                max3 = n
+        
+        return max1 if max3 == None else max3
+        
+    # time complexity: O(n)
+    # space complexity: O(1)
+    
