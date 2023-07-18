d41d17aae1a3423124a325bd3149258b9084c930
Kyle Bower <kylewbower@gmail.com>
2023-07-18 00:15:18 -0400

    Time: 82 ms (96.94%), Space: 22.9 MB (96.62%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,19 +1,17 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
+        # dynamic programming solution
+        
         # define answer array
         ans = [0]*(n+1)
         
         # iterate for each i, (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i
-        for i in range(n+1):
-            ans[i] = self.getOnes(i)
+        offset = 1
+        for i in range(1,n+1):
+            if i == 2*offset:
+                offset = i
+            ans[i] = ans[i-offset] + 1
         
         # return answer array
         return ans
-    
-    def getOnes(self, k: int) -> int:
-        res = 0
-        while k > 0:
-            res += k%2
-            k //= 2
-        return res
     
