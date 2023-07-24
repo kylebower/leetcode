a983eb322821ebbcce7f3676fc0ed3eec396d569
Kyle Bower <kylewbower@gmail.com>
2023-07-24 19:33:18 -0400

    Time: 246 ms (79.85%), Space: 18.2 MB (10.58%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeStars(self, s: str) -> str:
+        dq = deque()
+        for c in s:
+            if c == '*':
+                dq.pop()
+            else:
+                dq.append(c)
+        return ''.join(dq)
+    
