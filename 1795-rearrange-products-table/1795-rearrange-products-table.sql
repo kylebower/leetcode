6c8457d08c363f2a25c293f01b01d08d3f8b05e4
Kyle Bower <kylewbower@gmail.com>
2023-08-23 19:07:39 -0400

    Time: 1245 ms (45.55%), Space: 0B (100.00%) - LeetHub

--- 1795-rearrange-products-table/1795-rearrange-products-table.sql
+++ 1795-rearrange-products-table/1795-rearrange-products-table.sql
@@ -4,13 +4,13 @@ SELECT  product_id,
         store1 AS price
 FROM    Products
 WHERE   store1 IS NOT NULL
-UNION
+UNION ALL
 SELECT  product_id,
         'store2' AS store,
         store2 AS price
 FROM    Products
 WHERE   store2 IS NOT NULL
-UNION
+UNION ALL
 SELECT  product_id,
         'store3' AS store,
         store3 AS price
