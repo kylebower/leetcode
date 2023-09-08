f8b8850fb538ec67a9ca65c7f5db0fc84e8677a3
Kyle Bower <kylewbower@gmail.com>
2023-09-08 14:47:51 -0400

    Time: 1971 ms (26.38%), Space: 0B (100.00%) - LeetHub

--- 1251-average-selling-price/1251-average-selling-price.sql
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -3,7 +3,7 @@ SELECT
     u.product_id,
     IF(SUM(u.units) IS NOT NULL, ROUND(SUM(u.units * p.price)/SUM(u.units), 2), 0) AS average_price
 FROM
-    UnitsSold u INNER JOIN Prices p
+    UnitsSold u LEFT JOIN Prices p
     ON u.product_id = p.product_id AND (u.purchase_date BETWEEN p.start_date AND p.end_date)
 GROUP BY
     product_id
