d3434045460f4c3d803034c08c4ca82de6b5a6d3
Kyle Bower <kylewbower@gmail.com>
2023-09-08 14:47:07 -0400

    Time: 2009 ms (23.73%), Space: 0B (100.00%) - LeetHub

--- 1251-average-selling-price/1251-average-selling-price.sql
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
 SELECT
     u.product_id,
-    ROUND(SUM(u.units * p.price)/SUM(u.units), 2) AS average_price
+    IF(SUM(u.units) IS NOT NULL, ROUND(SUM(u.units * p.price)/SUM(u.units), 2), 0) AS average_price
 FROM
     UnitsSold u INNER JOIN Prices p
     ON u.product_id = p.product_id AND (u.purchase_date BETWEEN p.start_date AND p.end_date)
