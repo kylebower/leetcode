b71c164ef305adc2e4cd8f88926db926a8235baf
Kyle Bower <kylewbower@gmail.com>
2023-10-08 12:02:51 -0400

    Time: 3616 ms (9.84%), Space: 0B (100.00%) - LeetHub

--- 1907-count-salary-categories/1907-count-salary-categories.sql
+++ 1907-count-salary-categories/1907-count-salary-categories.sql
@@ -1,15 +1,15 @@
 # Write your MySQL query statement below
-SELECT 'Low Salary' AS CATEGORY,
+SELECT 'Low Salary' AS category,
     SUM(CASE WHEN income < 20000 THEN 1 ELSE 0 END) AS accounts_count
 FROM
     Accounts
 UNION
-SELECT 'Average Salary' AS CATEGORY,
+SELECT 'Average Salary' AS category,
     SUM(CASE WHEN income >= 20000 AND income <= 50000 THEN 1 ELSE 0 END) AS accounts_count
 FROM
     Accounts
 UNION
-SELECT 'High Salary' AS CATEGORY,
+SELECT 'High Salary' AS category,
     SUM(CASE WHEN income > 50000 THEN 1 ELSE 0 END) AS accounts_count
 FROM
     Accounts
