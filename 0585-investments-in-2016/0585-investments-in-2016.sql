b2cdbd1298b24f87444ad29a59d68dd957b64447
Kyle Bower <kylewbower@gmail.com>
2023-10-21 19:00:06 -0400

    Time: 1064 ms (47.82%), Space: 0B (100.00%) - LeetHub

---
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT
+    ROUND(SUM(tiv_2016), 2) AS tiv_2016
+FROM
+    Insurance A
+WHERE
+    (SELECT COUNT(*) FROM Insurance B WHERE A.lat = B.lat AND A.lon = B.lon) = 1
+    AND
+    (SELECT COUNT(*) FROM Insurance B WHERE A.tiv_2015 = B.tiv_2015) > 1
+    
