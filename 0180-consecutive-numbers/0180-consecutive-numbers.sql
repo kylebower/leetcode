095828aa784b23cd629804f605dc1cdf413b3695
Kyle Bower <kylewbower@gmail.com>
2023-10-27 21:20:57 -0400

    Time: 867 ms (87.75%), Space: 0B (100.00%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -2,7 +2,7 @@
 
 select distinct t.num as ConsecutiveNums
 from
-    (select *,
+    (select num,
         lead(num,1) over (order by id) as nxt,
         lead(num,2) over (order by id) as nxt2
     from Logs l) as t
