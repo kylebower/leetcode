389ef40093cdeb41bce41890aaa0ad910f905315
Kyle Bower <kylewbower@gmail.com>
2023-11-10 15:57:54 -0500

    Time: 65 ms (70.04%), Space: 16.2 MB (66.62%) - LeetHub

--- 2778-sum-of-squares-of-special-elements/2778-sum-of-squares-of-special-elements.py
+++ 2778-sum-of-squares-of-special-elements/2778-sum-of-squares-of-special-elements.py
@@ -4,6 +4,6 @@ class Solution:
         n = len(nums)
         for i, num in enumerate(nums):
             if n % (i+1) == 0:
-                res += num*num
+                res += num**2
         return res
     
