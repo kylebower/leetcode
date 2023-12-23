b2503c2db2dbda4598a578a5d75f65a4a1d89f59
Kyle Bower <kylewbower@gmail.com>
2023-12-22 21:54:41 -0500

    Time: 50 ms (90.95%), Space: 17.3 MB (6.27%) - LeetHub

---
+++ 2899-last-visited-integers/2899-last-visited-integers.py
@@ -0,0 +1,21 @@
+class Solution:
+    def lastVisitedIntegers(self, words: List[str]) -> List[int]:
+        k = 0
+        nums = []
+        nums_reverse = []
+        
+        res = []
+        
+        for i, w in enumerate(words):
+            if w == 'prev':
+                k += 1
+                if k > len(nums):
+                    res.append(-1)
+                else:
+                    res.append(nums[-k])
+            else:
+                k = 0
+                nums.append(int(w))
+        
+        return res
+    
