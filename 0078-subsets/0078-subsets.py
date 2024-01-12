21e3d270f5a1bbe075ed66f085e80cb5002513d1
Kyle Bower <kylewbower@gmail.com>
2024-01-12 14:19:23 -0500

    Time: 46 ms (26.17%), Space: 17.5 MB (27.15%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,16 +1,7 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        n = len(nums)
-        for k in range(2**n):
-            listj = []
-            binaryk = list(str(bin(k)))[2:]
-            binaryk.reverse()
-            for i, num in enumerate(nums):
-                if binaryk[i] == '1':
-                    listj.append(i)
-                if i == len(binaryk)-1:
-                    break
-            res.append([nums[j] for j in listj])
+        res = [[]]
+        for n in nums:
+            res += [x + [n] for x in res]
         return res
     
