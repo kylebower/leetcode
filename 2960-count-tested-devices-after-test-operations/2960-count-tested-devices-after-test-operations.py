2cafb07c8c3050363c3d10860411883ce4dd512e
Kyle Bower <kylewbower@gmail.com>
2024-02-08 13:09:56 -0500

    Time: 42 ms (99.84%), Space: 16.5 MB (17.63%) - LeetHub

--- 2960-count-tested-devices-after-test-operations/2960-count-tested-devices-after-test-operations.py
+++ 2960-count-tested-devices-after-test-operations/2960-count-tested-devices-after-test-operations.py
@@ -2,8 +2,8 @@ class Solution:
     def countTestedDevices(self, batteryPercentages: List[int]) -> int:
         n = len(batteryPercentages)
         tested_devices = 0
-        for i,bat in enumerate(batteryPercentages):
-            if batteryPercentages[i] > tested_devices:
+        for bat in batteryPercentages:
+            if bat > tested_devices:
                 tested_devices += 1
         return tested_devices
                 
