2ff390c5710c495eca3de8cb8b288c2cc77ccc01
Kyle Bower <kylewbower@gmail.com>
2024-03-02 14:55:31 -0500

    Time: 46 ms (24.04%), Space: 16.7 MB (41.95%) - LeetHub

--- 1103-distribute-candies-to-people/1103-distribute-candies-to-people.py
+++ 1103-distribute-candies-to-people/1103-distribute-candies-to-people.py
@@ -2,11 +2,10 @@ class Solution:
     def distributeCandies(self, candies: int, num_people: int) -> List[int]:
         num_candies = [0]*num_people
         current = 1
-        count = 0
         while candies > 0:
-            num_candies[count%num_people] += min(current, candies)
-            candies -= min(current, candies)
-            count += 1
+            candy_to_give = min(current, candies)
+            num_candies[(current-1)%num_people] += candy_to_give
+            candies -= candy_to_give
             current += 1
         return num_candies
     
