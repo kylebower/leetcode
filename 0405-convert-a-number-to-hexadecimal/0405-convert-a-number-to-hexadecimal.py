3413c2925ece210112166560bf41b9c23e7d131f
Kyle Bower <kylewbower@gmail.com>
2024-03-04 20:50:09 -0500

    Time: 33 ms (70.61%), Space: 16.5 MB (69.19%) - LeetHub

--- 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.py
+++ 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.py
@@ -1,5 +1,9 @@
 class Solution:
     def toHex(self, num: int) -> str:
+        # define dict
+        d = defaultdict(int)
+        for k in range(10,17):
+            d[k] = chr(ord('a') + k - 10)
             
         # base case:
         if num == 0:
@@ -11,16 +15,12 @@ class Solution:
         res = ''
         while num > 0:
             last_digit = num%16
-            res = self.hexidecimal(last_digit) + res
+            res = self.hexidecimal(last_digit, d) + res
             num //= 16
         
         return res
     
-    def hexidecimal(self, digit: int) -> str:
-        # define dict
-        d = defaultdict(int)
-        for k in range(10,17):
-            d[k] = chr(ord('a') + k - 10)
+    def hexidecimal(self, digit: int, d: dict) -> str:
         if digit < 10:
             return str(digit)
         else:
