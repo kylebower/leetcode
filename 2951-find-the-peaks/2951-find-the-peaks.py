0f13e221ffb4c55c4ff340f35bb5f5de92fa6ea2
Kyle Bower <kylewbower@gmail.com>
2024-03-20 08:10:30 -0400

    Time: 47 ms (66.01%), Space: 16.6 MB (47.36%) - LeetHub

--- 2951-find-the-peaks/2951-find-the-peaks.py
+++ 2951-find-the-peaks/2951-find-the-peaks.py
@@ -3,12 +3,9 @@ class Solution:
         n = len(mountain)
         peak_indices = []
         
-        for i, height in enumerate(mountain):
-            if i == 0 or i == n-1:
-                continue
-            else:
-                if height > mountain[i-1] and height > mountain[i+1]:
-                    peak_indices.append(i)
+        for i, height in enumerate(mountain[1:n-1]):
+            if height > mountain[i] and height > mountain[i+2]:
+                peak_indices.append(i+1)
         
         return peak_indices
     
